164659b8393c7808831319e039b350941ae7390b
shoponcom001 <shopon.com001@gmail.com>
2021-10-31 15:14:18 +0000

    page294

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,16 @@
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   if Rails.env.development? || Rails.env.test?
+#     provider :github, "b864badfeaaf7ab77695", "d8af2a39f626deba1836fc0195acb98802406dc1"
+#   else
+#     provider :github,
+#       Rails.application.credentials.github[:client_id],
+#       Rails.application.credentials.github[:client_secret]
+#   end
+# end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
-    provider :github, "b864badfeaaf7ab77695", "d8af2a39f626deba1836fc0195acb98802406dc1"
+    provider :github, "YOUR_CLIENT_ID", "YOUR_CLIENT_SECRET"
   else
     provider :github,
       Rails.application.credentials.github[:client_id],
